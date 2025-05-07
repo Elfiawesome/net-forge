1c3ba09a46135dc7a32a2fe23b05797b1ffd50a9
Elfiawesome23 <elfiawesome23@gmail.com>
2025-05-07 15:01:16 +0800

    Removed old map code

--- client/client.gd
+++ client/client.gd
@@ -3,10 +3,7 @@ class_name Client extends Node2D
 @onready var network_client_manager: NetworkClientManager = $NetworkClientManager
 @onready var top_overlay: ClientTopOverlay = $TopOverlay
 
-const MAP_SCENE := preload("res://client/map/local_map.tscn")
-
 var game_config: PersistanceManager.ServerConfig
-var map: LocalMap
 
 func _ready() -> void:
 	top_overlay.set_overlay_message("Loading into server...")
@@ -20,13 +17,6 @@ func connect_to_server(address: String, port: int, username: String) -> void:
 		"username": username,
 	})
 	network_client_manager.add_child(network_client_manager.connection)
-
-func set_map(new_map: LocalMap) -> void:
-	if map != null:
-		map.queue_free()
-	map = new_map
-	new_map.connection = network_client_manager.connection
-	add_child(new_map)
 
 func _on_packet_received(type: String, data: Array) -> void:
 	var handler := PacketHandlerClient.get_handler(type)
--- client/packet_handler/space/map/despawn_entity.gd
+++
@@ -1,9 +0,0 @@
-extends PacketHandlerClient
-
-func run(client: Client, data: Array) -> void:
-	if !Schema.is_valid(data, [TYPE_STRING]): return
-	if client.map == null: return
-	
-	var entity_id: String = data[0]
-	if client.map.entities.has(entity_id):
-		client.map.entities[entity_id].queue_free()
